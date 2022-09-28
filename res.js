8f65ea4a2eb97720b108b6ab4b07102ad9524dac
Vikrambharathi2 <113039725+Vikrambharathi2@users.noreply.github.com>
2022-09-28 19:40:17 +0530

    Add files via upload
    ..

---
+++ res.js
@@ -0,0 +1,144 @@
+ function lemonchicken()
+{ 
+ var total=0;
+ const price= "60";
+ var quantity=document.getElementById("lemonchickenquan").value;
+ if(quantity>0){
+ var total= price * quantity;
+ document.getElementById("lemonchickenbtn").innerHTML="Pay ₹"+total;
+}
+}
+lemonchicken();
+
+
+
+// fish
+
+function fish()
+{ 
+ var total=0;
+ const price= "60";
+ var quantity=document.getElementById("fishquan").value;
+ if(quantity>0){
+ var total= price * quantity;
+ document.getElementById("fishbtn").innerHTML="Pay ₹"+total;
+}
+}
+fish();
+
+// eggsandwich
+
+function eggsandwich()
+{ 
+ var total=0;
+ const price= "90";
+ var quantity=document.getElementById("eggsandwichquan").value;
+ if(quantity>0){
+ var total= price * quantity;
+ document.getElementById("eggsandwichbtn").innerHTML="Pay ₹"+total;
+}
+}
+eggsandwich();
+
+
+// muttonsandwich
+function muttonsandwich()
+{ 
+ var total=0;
+ const price= "250";
+ var quantity=document.getElementById("muttonsandwichquan").value;
+ if(quantity>0){
+ var total= price * quantity;
+ document.getElementById("muttonsandwichbtn").innerHTML="Pay ₹"+total;
+}
+}
+muttonsandwich();
+
+// Chickenbiryani
+function Chickenbiryani()
+{ 
+ var total=0;
+ const price= "140";
+ var quantity=document.getElementById("Chickenbiryaniquan").value;
+ if(quantity>0){
+ var total= price * quantity;
+ document.getElementById("Chickenbiryanibtn").innerHTML="Pay ₹"+total;
+}
+
+}
+Chickenbiryani();
+
+
+
+// veg items
+
+
+function lemonrice()
+{ 
+ var total=0;
+ const price= "40";
+ var quantity=document.getElementById("lemonricequan").value;
+ if(quantity>0){
+ var total= price * quantity;
+ document.getElementById("lemonricebtn").innerHTML="Pay ₹"+total;
+}
+
+}
+lemonrice();
+
+// tomatorice
+function tomatorice()
+{ 
+ var total=0;
+ const price= "70";
+ var quantity=document.getElementById("tomatoricequan").value;
+ if(quantity>0){
+ var total= price * quantity;
+ document.getElementById("tomatoricebtn").innerHTML="Pay ₹"+total;
+}
+
+}
+tomatorice();
+
+// curdrice
+function curdrice()
+{ 
+ var total=0;
+ const price= "50";
+ var quantity=document.getElementById("curdricequan").value;
+ if(quantity>0){
+ var total= price * quantity;
+ document.getElementById("curdricebtn").innerHTML="Pay ₹"+total;
+}
+
+}
+curdrice();
+
+// idlidosa
+
+function idlidosa()
+{ 
+ var total=0;
+ const price= "50";
+ var quantity=document.getElementById("idlidosaquan").value;
+ if(quantity>0){
+ var total= price * quantity;
+ document.getElementById("idlidosabtn").innerHTML="Pay ₹"+total;
+}
+
+}
+idlidosa();
+
+// vegbiryani
+function vegbiryani()
+{ 
+ var total=0;
+ const price= "90";
+ var quantity=document.getElementById("vegbiryaniquan").value;
+ if(quantity>0){
+ var total= price * quantity;
+ document.getElementById("vegbiryanibtn").innerHTML="Pay ₹"+total;
+}
+
+}
+vegbiryani();
